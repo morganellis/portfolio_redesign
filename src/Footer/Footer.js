aa5e5e10d75199616904c8579425bf9fb03ca73a
Morgan-Ellis <Morgan.Ellis@ariix.com>
2020-07-09 22:38:42 -0600

    portfolio redesign first draft done

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,9 +4,16 @@ import "./footer.scss";
 export default function Footer() {
   return (
     <div className="footer-wrap">
-      <p className="para">LinkedIn</p>
-      <p className="para">GitHub</p>
-      <p className="para">morganellis.dev@gmail.com</p>   
+      <div className="col-1 col">
+        <a href="https://www.linkedin.com/in/morgan-shiloh-ellis/" target="_blank" rel="noopener noreferrer"><img className="social-icon" src={require("../assets/img/main/linkedin-gray.png")} alt="LinkedIn" /></a>
+        <a href="https://github.com/morganellis" target="_blank" rel="noopener noreferrer"><img className="social-icon" src={require("../assets/img/main/github-gray.png")} alt="GitHub" /></a>
+        <a href="mailto: morganellis.dev@gmail.com"><img className="social-icon" src={require("../assets/img/main/email-gray.png")} alt="Email" /></a>
+      </div>
+
+      <div className="col">
+        <p className="para">© 2020 Morgan Ellis</p>
+        </div>
+
     </div>
   )
 }
