d4527c9b7ab3cd511ff6ea46037cd9925547a76d
Morgan-Ellis <Morgan.Ellis@ariix.com>
2020-07-15 19:18:00 -0600

    stuff

--- src/Projects/Diamonds/Diamonds.js
+++ src/Projects/Diamonds/Diamonds.js
@@ -21,19 +21,19 @@ export default function Diamonds(props) {
         <p className="title">ARIIX DIAMONDS</p>
       </div>
 
-      <div className="vid-sect section">
-        <div className="vid-wrap">
-          <img className="desktop-overlay" src={require("../../assets/img/main/desktop-no-bkg.png")} alt="" />
-          {checkIfMobile() === true ?
-            <iframe title="diamonds-education" src="https://giphy.com/embed/XH55z6fv2fWwqgfjmk" frameBorder="0" class="celeb-gif gif vid giphy-embed" allowFullScreen></iframe>
-            :
-            <video className="vid" loop autoPlay muted><source src="https://i.imgur.com/Rvm71fQ.mp4" type="video/ogg" />Your browser does not support the video tag.</video>
-          }
-        </div>
-      </div>
-
-      {/* <div className="project-details">
+      <div className="project-details">
         <div className="vid-sect section">
+          <div className="vid-wrap">
+            <img className="desktop-overlay" src={require("../../assets/img/main/desktop-no-bkg.png")} alt="" />
+            {checkIfMobile() === true ?
+              <iframe title="diamonds-education" src="https://giphy.com/embed/XH55z6fv2fWwqgfjmk" frameBorder="0" class="celeb-gif gif vid giphy-embed" allowFullScreen></iframe>
+              :
+              <video className="vid" loop autoPlay muted><source src="https://i.imgur.com/Rvm71fQ.mp4" type="video/ogg" />Your browser does not support the video tag.</video>
+            }
+          </div>
+        </div>
+        
+        {/* <div className="vid-sect section">
           <div className="vid-wrap">
             <img className="desktop-overlay" src={require("../../assets/img/main/desktop-no-bkg.png")} alt="" />
             {checkIfMobile() === true ?
@@ -104,7 +104,7 @@ export default function Diamonds(props) {
           </div>
         </div> */}
 
-        <div className="section">
+        <div className="below-vid-sect section">
           <img className="fullpage" src={require("../../assets/img/work/diamonds/diamonds-education.png")} alt="" />
         </div>
 
