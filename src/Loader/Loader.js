d9c0c0d52fa1228d47bfd4dde5829be084ebeaec
Morgan-Ellis <Morgan.Ellis@ariix.com>
2020-07-24 16:29:58 -0600

    basic loader

--- src/Loader/Loader.js
+++ src/Loader/Loader.js
@@ -3,16 +3,18 @@ import "./loader.scss";
 
 export default function Loader() {
   return (
+
+    <div id="loadWrap"><div className="loading-bkg"></div><div id="loading" className="project-loading-wrap"><div className="loading-ring"><div></div><div></div><div></div><div></div></div></div></div>
     // <div className="load-wrap">
 
-    <div align="center" className="fond">
-      <div className="contener_general">
-        <div className="contener_mixte"><div className="ballcolor ball_1">&nbsp;</div></div>
-        <div className="contener_mixte"><div className="ballcolor ball_2">&nbsp;</div></div>
-        <div className="contener_mixte"><div className="ballcolor ball_3">&nbsp;</div></div>
-        <div className="contener_mixte"><div className="ballcolor ball_4">&nbsp;</div></div>
-      </div>
-    </div>
+    // <div align="center" className="fond">
+    //   <div className="contener_general">
+    //     <div className="contener_mixte"><div className="ballcolor ball_1">&nbsp;</div></div>
+    //     <div className="contener_mixte"><div className="ballcolor ball_2">&nbsp;</div></div>
+    //     <div className="contener_mixte"><div className="ballcolor ball_3">&nbsp;</div></div>
+    //     <div className="contener_mixte"><div className="ballcolor ball_4">&nbsp;</div></div>
+    //   </div>
+    // </div>
     // </div>
   )
 }
